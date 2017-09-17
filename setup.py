cb87824b84bf385a18275f523fd6742c5c975b0e
champax <champagnac.laurent@gmail.com>
2017-09-17 20:58:03 +0200

    Short description / readme

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     author=p_author,
     author_email=p_email,
     url=p_url,
-    description=p_name + " APIs",
+    description="Simple MYSQL client Apis",
 
     # Version, format : Major.Minor.Revision
     version=p_version,
