033479aa3e2b3884d8f2ced2873bdc21cd97e6a5
champax <champagnac.laurent@gmail.com>
2019-08-27 10:44:13 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ p_name = "pysolmysql"
 p_author = "Laurent Champagnac"
 p_email = "champagnac.laurent@gmail.com"
 p_url = "https://knock.center"
-p_version = "1.1.3"
+p_version = "1.1.4"
 
 # Load
 req_list, dep_list = requirement_read("requirements.txt")
